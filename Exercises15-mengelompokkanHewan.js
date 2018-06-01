f2c10cb538611a3b6f716014fb4c744eab4bf0d2
Susan Nio <susan.wen88@gmail.com>
2018-06-02 02:28:02 +0700

    Adding solutions to Exercises 15

--- Exercises15-mengelompokkanHewan.js
+++ Exercises15-mengelompokkanHewan.js
@@ -1,3 +1,31 @@
+/* Solution 1 - using slice() and for ... of */
+function groupAnimals(animals){
+  animals.sort();
+  var target = animals[0].slice(0,1);
+  var smallBox = [];
+  var bigBox = [];
+  for(var animal of animals){
+    if(animal[0] === target){
+        smallBox.push(animal);
+    } else {
+      target = animal.slice(0,1);
+      bigBox.push(smallBox);
+      smallBox=[];
+      smallBox.push(animal);
+    }  
+  }
+  bigBox.push(smallBox);
+  return bigBox;
+}
+
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+//[['anoa', 'ayam'], ['cacing'], ['kancil', 'kuda']]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+// [['anoa', 'ayam'],['cacing','cicak'],['kancil', 'kuda'],['unta']]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'macan', 'beruang', 'zebra', 'tupai', 'capung', 'kupu-kupu'])); 
+//[['anoa','ayam'],['beruang'],['cacing','capung'],['kancil','kuda','kupu-kupu'],['macan'],['tupai'],['zebra']];
+
+/* Solution 2 - nested loops, else if, and breaks
 function groupAnimals(groupAnimals) {
   groupAnimals.sort()
   var groups=[[]];
@@ -37,11 +65,27 @@ function groupAnimals(groupAnimals) {
   }
   return groups;
 }
+*/
 
-// TEST CASES
-console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
-// [['anoa', 'ayam'], ['cacing'], ['kancil', 'kuda']]
-console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
-// [['anoa', 'ayam'],['cacing','cicak'],['kancil', 'kuda'],['unta']]
-console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'macan', 'beruang', 'zebra', 'tupai', 'capung', 'kupu-kupu'])); 
-// [['beruang'],['cacing','capung'],['kancil','kuda','kupu-kupu'],['macan'],['tupai'],['zebra']]
+/* SOLUTION 3 - using index and for loops
+function groupAnimals(animals) {
+  animals.sort();
+  var target = animals[0][0];
+  var boxKecil=[];
+  var boxBesar=[];
+  
+  for(var i=0; i<animals.length; i++){
+    if(animals[i][0] === target){
+      boxKecil.push(animals[i]);
+    } else {
+      target = animals[i][0];
+      boxBesar.push(boxKecil);
+      boxKecil = [];
+      boxKecil.push(animals[i]);
+    }
+  }
+  boxBesar.push(boxKecil);
+  return boxBesar;
+}
+*/
+
